83e2c7a3878456595182d51455fcc99d66ca7668
Eridanus Sora <sora@sound.moe>
2019-03-21 19:41:41 +0800

    add auto deploy

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,18 @@ module.exports = {
         electronBuilder: {
             builderOptions: {
                 appId: 'moe.sound.sora.monita',
+                artifactName: "monita-${os}.${ext}",
                 productName: 'Monita',
                 win: {
                     target: 'msi',
                     // icon: 'build/icon.ico'
+                },
+                mac: {
+                    category: 'public.app-category.developer-tools',
+                    target: ['dmg', 'zip']
+                },
+                linux: {
+                    target: ['deb', 'appImage', 'rpm']
                 }
             }
         }
