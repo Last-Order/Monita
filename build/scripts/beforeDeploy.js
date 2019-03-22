9006102207c837831de9d77b91fe1319c1b00574
Eridanus Sora <sora@sound.moe>
2019-03-22 16:29:51 +0800

    fix ci

--- build/scripts/beforeDeploy.js
+++ build/scripts/beforeDeploy.js
@@ -3,8 +3,10 @@ const path = require('path');
 const exec = util.promisify(require('child_process').exec);
 
 (async () => {
+    await exec(`ls -a ${path.resolve(__dirname, '../../dist_electron')}`);
     for (const ext of ['dmg', 'zip', 'appImage', 'deb', 'rpm', 'msi']) {
         try {
+            console.log(`mv ${path.resolve(__dirname, '../../dist_electron/*.' + ext)} ${path.resolve(__dirname, '../../dist_binary/')}`);
             await exec(`mv ${path.resolve(__dirname, '../../dist_electron/*.' + ext)} ${path.resolve(__dirname, '../../dist_binary/')}`);
         } catch (e) {
             //
