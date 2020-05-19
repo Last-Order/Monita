6570555e9de77890f83193d0b68425936a1ccd8b
Eridanus Sora <sora@sound.moe>
2020-05-19 21:04:35 +0800

    fix sr parser

--- src/services/parsers/showroom.js
+++ src/services/parsers/showroom.js
@@ -6,25 +6,31 @@ export default async function parse(url) {
             const page = await axios.get(url);
             const doc = parser.parseFromString(page.data, "text/html");
             const videoInfo = JSON.parse(doc.getElementById('js-initial-data').dataset.json);
-            if (!videoInfo.streamingUrlHls) {
+            const roomId = videoInfo.roomId;
+            const streamingInfoUrl = `https://www.showroom-live.com/api/live/streaming_url?room_id=${roomId}`;
+            const streamingInfo = (await axios.get(streamingInfoUrl)).data;
+            if (!streamingInfo.streaming_url_list) {
                 resolve({
                     status: 'wait',
                     type: 'hls',
                     streams: [],
                     title: doc.title
-                })
+                });
             } else {
+                // const playlistUrl = streamingInfo.streaming_url_list.find(p => p.is_default === true).url;
+                // console.log(playlistUrl)
                 resolve({
                     status: 'playing',
                     type: 'hls',
-                    streams: [{
-                        name: '默认',
-                        url: videoInfo.streamingUrlHls
-                    }],
+                    streams: streamingInfo.streaming_url_list.filter(p => p.is_default === true).map(i => {
+                        return {
+                            name: i.label,
+                            url: i.url
+                        }
+                    }),
                     title: doc.title
                 });
-            }
-            
+            }            
         } catch (e) {
             reject(e);
         }
